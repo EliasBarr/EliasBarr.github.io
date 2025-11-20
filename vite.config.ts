536ef795018fe1c7b17cbf7f1258e352af57d91a
Christian Garcerant <christian.garcerant@uac.edu.co>
2025-11-20 16:12:01 -0500

    Add comment placeholder in vite.config.ts for future reference

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
-
+//
 // https://vite.dev/config/
 export default defineConfig({
   base: '/', // GitHub Pages base path para EliasBarr.github.io (sitio de usuario)
